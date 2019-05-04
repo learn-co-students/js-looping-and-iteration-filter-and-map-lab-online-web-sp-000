c4c2db8c71bdb9c1f960a997b129bdee36661dbb
Jcg408 <jcg321@comcas.net>
2019-05-04 19:43:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Code your solution here:
+
 function driversWithRevenueOver(array, num) {
    const over =  array.filter(arr => arr.revenue > num);
     return over;
-
 };
 
 function driverNamesWithRevenueOver(array, num){
@@ -20,5 +19,5 @@ function exactMatch(array, obj) {
 
 function exactMatchToList(array, obj) {
     return exactMatch(array, obj).map(attr => attr.name);
-}
+};
 
