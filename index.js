2af972cf532bc2b4ec9065d37512b978b3a44af8
jonmackeyparty <jon@jonmackey.party>
2019-05-19 09:26:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here:
+function driversWithRevenueOver(array, amount) {
+    const newArray = array.filter(driver => driver.revenue > amount);
+    return newArray;
+}
+
+function driverNamesWithRevenueOver(array, amount) {
+    const newArray = array.filter(driver => driver.revenue > amount).map(driver => driver.name);
+    return newArray;
+}
+
+function exactMatch(array, value){
+  const newArray = array.filter(driver => driver.name === value.name || driver.revenue === value.revenue);
+  return newArray;
+}
+
+function exactMatchToList(array, value) {
+  const newArray = array.filter(driver => driver.name === value.name || driver.revenue === value.revenue).map(driver => driver.name);
+  return newArray;
+}
