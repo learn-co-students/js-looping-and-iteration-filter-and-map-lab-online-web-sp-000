060e63e2f90effdf0a448c3fd9df90587ce4fcba
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-23 03:46:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
 // Code your solution here:
 
-// function driversWithRevenueOver(arrayOfDriverObjects, revenueNumber) {
-//   return arrayOfDriverObjects.filter(function(driverObject) {
-//     return driverObject.revenue > revenueNumber
-//   })
-// }
-
-// function driverNamesWithRevenueOver(arrayOfDriverObjects, revenueNumber) {
-//   return driversWithRevenueOver(arrayOfDriverObjects, revenueNumber).map(function(driverObject) {
-//     return driverObject.name
-//   })
-// }
-
-// function exactMatch (arrayOfDriverObjects, unknownObject) {
-//   return arrayOfDriverObjects.filter(function(driverObject) {
-//     // let matches = []
-//     let matches
-//     for (const key in unknownObject) {
-//       matches = driverObject[key] === unknownObject[key]
-//     }
-//     return matches
-//   })
-// }
-
-// function exactMatchToList (arrayOfDriverObjects, unknownObject) {
-//   return exactMatch (arrayOfDriverObjects, unknownObject)
-//     .map(function (driverObject) {
-//       return driverObject.name;
-//     });
-// }
-
-function driversWithRevenueOver(array, number) {
-  return array.filter(element => element.revenue > number);
+function driversWithRevenueOver(arrayOfDriverObjects, number) {
+  return arrayOfDriverObjects.filter(driverObject => driverObject.revenue > number);
 }
 
 
 function driverNamesWithRevenueOver(array, number) {
-  return driversWithRevenueOver(array, number).map(element => element.name);
+  return driversWithRevenueOver(array, number).map(driverObject => driverObject.name);
 }
 
 
-function exactMatch(array, object) {
-  return array.filter(element => Object.keys(element).includes(Object.keys(object)[0]) && 
-   element[Object.keys(object)[0]] === object[Object.keys(object)[0]]
-  );
+// function exactMatch(arrayOfDriverObjects, unknownObject) {
+//   return arrayOfDriverObjects.filter(driverObject => Object.keys(driverObject).includes(Object.keys(unknownObject)[0]) && 
+//   driverObject[Object.keys(unknownObject)[0]] === unknownObject[Object.keys(unknownObject)[0]]
+//   );
+// }
+
+function exactMatch (arrayOfDriverObjects, unknownObject) {
+  return arrayOfDriverObjects.filter(function(driverObject) {
+    let matches
+    for (const key in unknownObject) {
+      matches = driverObject[key] === unknownObject[key]
+    }
+    return matches
+  })
 }
 
 
-function exactMatchToList(array, object) {
-  return exactMatch(array, object).map(element => element.name);
+function exactMatchToList(arrayOfDriverObjects, object) {
+  return exactMatch(arrayOfDriverObjects, object).map(driverObject => driverObject.name);
 }
