73f42931c8b37a415381835f1f2f5c49d1aadc34
Ramon Chavez <rachavez1974@gmail.com>
2019-10-05 21:07:04 -0400

    Finish js-looping-and-iteration-filter-and-map-lab-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue){
+   return drivers.filter((driver) => {
+    return driver.revenue > revenue;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  let names = drivers.filter((driver) => {
+    if (driver.revenue > revenue){
+    return driver.name;
+    }
+  });
+  return [...new Set(names.map(name => name.name))];
+}
+
+function exactMatch(drivers, driverObj){
+  let attribute = Object.keys(driverObj)[0];
+  return drivers.filter((driver) => {
+    if (driverObj[attribute] === driver[attribute]){
+      return driver;
+    }
+  }); 
+}
+
+function exactMatchToList(drivers, driverObj){
+  let attribute = Object.keys(driverObj)[0];
+  let names = drivers.filter((driver) => {
+    if (driverObj[attribute] === driver[attribute]){
+      return driver.name;
+    }
+  }); 
+  return names.map(name => name.name);
+}
+
+let drivers = [{ name: 'Sally',   revenue: 400 },
+      { name: 'Annette', revenue: 200 },
+      { name: 'Jim',     revenue: 150 },
+      { name: 'Sally',   revenue: 200 }];
+
+console.log(exactMatchToList(drivers, drivers[0]));
+// driversWithRevenueOver(drivers, 250);
